ab2ea21ad190e92cbec86b1ae76f8a620a619ff9
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-08-03 10:44:57 -0500

    subqueries

--- join_exercises.sql
+++ join_exercises.sql
@@ -15,15 +15,16 @@ FROM employees as e
               ON dm.emp_no = e.emp_no
          JOIN departments as d
               ON d.dept_no = dm.dept_no
-WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
+WHERE dm.to_date = '9999-01-01'
+  AND e.gender = 'F'
 order by dept_name;
 
 SELECT t.title AS Title, COUNT(t.emp_no) AS Count
 FROM titles AS t
-    JOIN dept_emp as de
-ON t.emp_no = de.emp_no
-    JOIN departments as d
-    ON de.dept_no = d.dept_no
+         JOIN dept_emp as de
+              ON t.emp_no = de.emp_no
+         JOIN departments as d
+              ON de.dept_no = d.dept_no
 WHERE de.dept_no = 'd009'
   AND t.to_date = '9999-01-01'
   AND de.to_date = '9999-01-01'
